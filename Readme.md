7cf2d68ea7673b2b11d64a68e92f9959b7173f76
mahsa <mahsaadlooo@gmail.com>
2023-07-24 18:04:57 +0430

    uppercase method

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Switch lowercase to uppercase.
+
+Use ToUpperCase method to change text from lowercase to uppercase. Also we use prompt to get data from users and show the result in console. 
---
+++ index.js
@@ -0,0 +1,12 @@
+// const text= prompt(" write something here to change it to UPPERCASE : ");
+// const result = text.toUpperCase();
+
+// console.log(result);
+
+function toupper() {
+    const text = prompt(" write something here to change it to UPPERCASE :");
+    const result = text.toUpperCase();
+    return(result);
+}
+
+console.log(toupper());
