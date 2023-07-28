b5ac46925bf1811d111fea360af6bc3362ba01f6
mahsa <mahsaadlooo@gmail.com>
2023-07-28 16:00:09 +0430

    existing a word in  a sentence

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Existing a word in a sentence
+
+In this project we use includes() method in a function to see if the words that users looking for in their sentences are exist or not. And also if they forget to fill inputs, they got error.
---
+++ index.js
@@ -0,0 +1,17 @@
+function findtext() {
+    let sentences = prompt("Enter your sentences please: ");
+    let findword = prompt("What are you looking for?");
+    if (!sentences) {
+        return console.log(" something is wrong ! please try again !");
+    }
+    else{
+        if (sentences.includes(findword)) {
+            return console.log("Word " + findword + " is Exist.");
+        } else {
+            return console.log("NOT FOUND !!!");
+        }
+    }
+  }
+  
+  findtext();
+  
